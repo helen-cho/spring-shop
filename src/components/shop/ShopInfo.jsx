19c8b1db4538be4d405c4a707b674dd7360f8df2
helen-cho <hd9536@daum.net>
2023-11-27 12:16:37 +0900

    ver03

--- src/components/shop/ShopInfo.jsx
+++ src/components/shop/ShopInfo.jsx
@@ -2,14 +2,15 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {Row, Col, Button} from 'react-bootstrap'
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
 const ShopInfo = () => {
     const {pid} = useParams();
     const [shop, setShop] = useState('');
-    const {title, maker, image, fmtprice, fmtdate} = shop;
+    const {title, maker, image, fmtprice, fmtdate, ucnt} = shop;
     const getShop = async() => {
-        const res = await axios(`/shop/info/${pid}`);
-        //console.log(res.data);
+        const res = await axios(`/shop/info/${pid}?uid=${sessionStorage.getItem("uid")}`);
         setShop(res.data);
     }
 
@@ -25,7 +26,12 @@ const ShopInfo = () => {
                     <img src={`/display?file=${image}`} width="90%" className='image'/>
                 </Col>
                 <Col className='mt-3'>
-                    <h5>[{pid}] {title}</h5>
+                    <h5>
+                        [{pid}] {title}
+                        <span className='heart mx-2'>
+                            {ucnt === 0 ? <FaRegHeart/> : <FaHeart/>}
+                        </span>
+                    </h5>
                     <hr/>
                     <div>가격: {fmtprice}원</div>
                     <div>제조사: {maker}</div>
