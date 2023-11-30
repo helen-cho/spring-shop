aeb98e1fc4b42afe64f1bdbe1ba57d22ca7edfed
helen-cho <hd9536@daum.net>
2023-11-30 13:21:18 +0900

    ver06

--- src/components/admin/OrderList.jsx
+++ src/components/admin/OrderList.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Table, Row, Col, InputGroup, Form, Button } from 'react-bootstrap';
 import "../Pagination.css";
 import Pagination from "react-js-pagination";
+import ModalOrder from '../order/ModalOrder';
 
 const OrderList = () => {
     const [list, setList] = useState([]);
@@ -40,6 +41,17 @@ const OrderList = () => {
         setList(data);
     }
 
+    const onClickStatus = async(oid, status) => {
+        await axios.post("/purchase/update/status", {oid, status});
+        alert("상태변경!");
+        getList();
+    } 
+
+    const onChangeKey = (e) => {
+        setQuery("");
+        setKey(e.target.value);
+    }
+
     return (
         <div className='my-5'>
             <h1 className='text-center mb-5'>주문관리</h1>
@@ -47,14 +59,28 @@ const OrderList = () => {
                 <Col md={5}>
                     <form onSubmit={onSumbit}>
                         <InputGroup>
-                            <Form.Select onChange={(e)=>setKey(e.target.value)}>
+                            <Form.Select onChange={onChangeKey}>
                                 <option value="oid">주문번호</option>
                                 <option value="uname">주문자명</option>
                                 <option value="uid">아이디</option>
                                 <option value="address1">배송지</option>
                                 <option value="phone">전화번호</option>
+                                <option value="status">주문상태</option>
                             </Form.Select>
-                            <Form.Control placeholder='검색어' onChange={(e)=>setQuery(e.target.value)}/>
+                            {key === 'status' ?
+                                <Form.Select onChange={(e)=>setQuery(e.target.value)}>
+                                    <option value="">모든주문</option>
+                                    <option value="0">결제확인중</option>
+                                    <option value="1">결제완료</option>
+                                    <option value="2">배송준비중</option>
+                                    <option value="3">배송중</option>
+                                    <option value="4">배송완료</option>
+                                    <option value="5">주문완료</option>
+                                </Form.Select>
+                                :
+                                <Form.Control placeholder='검색어' value={query}
+                                    onChange={(e)=>setQuery(e.target.value)}/>
+                            }
                             <Button type="submit">검색</Button>
                             <span className='mt-2 ms-2'>검색수:{total}건</span>
                         </InputGroup>
@@ -80,6 +106,7 @@ const OrderList = () => {
                         <td>주문자명</td>
                         <td>배송지</td>
                         <td>전화번호</td>
+                        <td>주문상품</td>
                         <td>주문상태</td>
                     </tr>
                 </thead>
@@ -90,6 +117,7 @@ const OrderList = () => {
                         <td>{p.uname}({p.uid})</td>
                         <td>{p.address1}</td>
                         <td>{p.phone}</td>
+                        <td><ModalOrder p={p}/></td>
                         <td>
                             <InputGroup>
                                 <Form.Select value={p.status} onChange={(e)=>onChangeStatus(e, p.oid)}>
@@ -100,7 +128,7 @@ const OrderList = () => {
                                     <option value="4">배송완료</option>
                                     <option value="5">주문완료</option>
                                 </Form.Select>
-                                <Button>변경</Button>
+                                <Button onClick={()=>onClickStatus(p.oid, p.status)}>변경</Button>
                             </InputGroup>
                         </td>
                     </tr>
